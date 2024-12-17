f4ae2018f27261f0c4aa3da56fe7449d13fe254a
Nik Dunna <nik.dunna@gmail.com>
2024-12-17 18:59:18 -0500

    Post cards complete, edit/delete buttons added (no functionality). TODO: full implement task 2

--- src/components/PostList.jsx
+++ src/components/PostList.jsx
@@ -22,14 +22,22 @@ export default function PostList() {
   });
 
   return (
-    <div className="flex items-center justify-center flex-col max-w-screen-md mx-auto my-4">
+    <div className="flex items-center justify-center flex-col max-w-screen-md mx-auto my-4 bg-newell-gray opacity-80 p-4 text-white rounded-md">
       <p className="text-2xl">Post Feed</p>
       <ul>
         {posts.map((post) => (
-          <div className="flex flex-col py-3">
+          <div className="flex flex-col p-4 bg-newell-blue rounded-lg m-4">
             <li key={post.id}>
               <p className="text-xl underline">{post.title}</p>
               <p className="">{post.body}</p>
+              <div className="flex flex-row justify-start items-centen space-x-10 text-lg m-2">
+                <button className="bg-newell-gray w-20 h-10 rounded-md">
+                  Edit
+                </button>
+                <button className="bg-red-500 w-20 h-10 rounded-md">
+                  Delete
+                </button>
+              </div>
             </li>
           </div>
         ))}
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "newell-blue": "#298FC2",
+        "newell-gray": "#696158",
+      },
+    },
   },
   plugins: [],
-}
-
+};
